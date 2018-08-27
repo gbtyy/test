e534e0291c86a86003a9c4034a226de8ad9b7a35
gaobotao <gaobotao@migu.cn>
2018-08-27 16:01:12 +0800

    1、修改test.java

--- src/com/Test.java
+++ src/com/Test.java
@@ -1,10 +1,11 @@
 package com;
 
 public class Test {
-    private int i;
+    private int i1;
     private int j;
     private int m;
     private int k2;
     private int n;
-    private int o;	private int c2;
+    private int o;
+	private int c2;
 }
